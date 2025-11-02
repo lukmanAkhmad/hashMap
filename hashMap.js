8739ecfd93ec02a357be61830b2035b20aae103f
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-11-02 07:13:59 +0700

    feat: method hash

--- hashMap.js
+++ hashMap.js
@@ -2,5 +2,14 @@ function HashMap() {
   let loadFactor = 0.75;
   let capacity = 16;
 
-  
+  function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 }
