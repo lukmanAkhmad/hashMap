8edcbd2fdbd2b15260ea66007836eebf068af8cf
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-11-09 10:41:23 +0700

    feat: method feat

--- hashMap.js
+++ hashMap.js
@@ -1,3 +1,5 @@
+import { LinkedList } from "./linked-lists.js";
+
 function HashMap() {
   let loadFactor = 0.75;
   let capacity = 16;
@@ -15,7 +17,51 @@ function HashMap() {
     return hashCode;
   };
 
+  const createList = LinkedList();
+  const set = (key, value) => {
+    const hashCode = hash(key);
+
+    if (buckets[hashCode] === undefined) {
+      const newCreateList = LinkedList();
+
+      newCreateList.append({ key, value });
+      buckets.splice(hashCode, 1, newCreateList.valueLinkedLists());
+      createList.removeLists();
+      createList.append(buckets[hashCode].LLvalue);
+    } else if (buckets[hashCode] !== undefined) {
+      const keyValue = createList.searchKey(key);
+      const indxKeyValue = createList.find(key);
+
+      console.log(key === keyValue);
+      console.log(indxKeyValue);
+      if (key === keyValue) {
+        console.log("calling removeAt");
+        createList.removeAt(indxKeyValue);
+      }
+
+      createList.append({ key, value });
+      buckets.splice(hashCode, 1, createList.valueLinkedLists());
+    }
+    console.log(createList.logKeyLL());
+  };
+
+  const showBuckets = () => buckets;
+
   return {
     hash,
+    set,
+    showBuckets,
   };
 }
+
+const test = HashMap();
+test.set("Carlos", "I am the old value.");
+test.set("Rama", "I am the old value.");
+test.set("Sita", "I am the new value.");
+test.set("yono", "I am the old value.");
+test.set("yono", "I am the new value.");
+test.set("yono", "I .");
+test.set("delapan", "I am");
+test.set("sembilan", "I am");
+
+console.log(...test.showBuckets());
---
+++ linked-lists.js
@@ -0,0 +1,244 @@
+function LinkedList() {
+  let headNode = null;
+
+  const append = (value) => {
+    if (headNode === null) {
+      headNode = CreateNode(value);
+    } else {
+      let tempNode = headNode;
+      while (tempNode.nextNode !== null) {
+        tempNode = tempNode.nextNode;
+      }
+      tempNode.nextNode = CreateNode(value);
+    }
+  };
+
+  const prepend = (value) => (headNode = CreateNode(value, headNode));
+
+  const size = () => {
+    let count = 0;
+    let tempNode = headNode;
+    while (tempNode !== null) {
+      count++;
+      tempNode = tempNode.nextNode;
+    }
+    return count;
+  };
+
+  const head = () => {
+    if (headNode === null) return null;
+
+    return headNode.value;
+  };
+
+  const tail = () => {
+    if (headNode === null) return null;
+
+    let tempNode = headNode;
+    while (tempNode.nextNode !== null) {
+      tempNode = tempNode.nextNode;
+    }
+    return tempNode.value;
+  };
+
+  const at = (index) => {
+    if (headNode === null) return null;
+
+    const sizeLinkedList = size();
+    const sizeLinkedListMinusOne = sizeLinkedList - 1;
+    let tempNode = headNode;
+
+    if (index < 0 || index > sizeLinkedListMinusOne) {
+      return "Index start at 0, and the index must be within the bounds of the linked list size.";
+    }
+
+    for (let i = 0; i < index; i++) {
+      tempNode = tempNode.nextNode;
+    }
+    return tempNode.value;
+  };
+
+  const pop = () => {
+    if (headNode === null) return "Cannot delete, linked list is empty";
+
+    let cur = headNode;
+    let prev = null;
+
+    while (cur.nextNode !== null) {
+      prev = cur;
+      cur = cur.nextNode;
+    }
+
+    if (prev === null) {
+      headNode = null;
+      return cur.value;
+    }
+    prev.nextNode = null;
+    return cur.value;
+  };
+
+  const contains = (value) => {
+    if (headNode === null) return "Linked list is empty";
+
+    let tempNode = headNode;
+
+    while (tempNode !== null) {
+      if (tempNode.value === value) return true;
+      tempNode = tempNode.nextNode;
+    }
+
+    if (tempNode === null) return false;
+  };
+
+  const find = (value) => {
+    if (headNode === null) return "Linked list is empty";
+
+    let tempNode = headNode;
+    let indxNode = 0;
+
+    while (tempNode !== null) {
+      if (tempNode.LLvalue.key === value) return indxNode;
+      indxNode++;
+      tempNode = tempNode.nextNode;
+    }
+
+    if (tempNode === null) return null;
+  };
+
+  const toString = () => {
+    let tempNode = headNode;
+    let str = "";
+    while (tempNode !== null) {
+      str += `( ${tempNode.value} ) -> `;
+      tempNode = tempNode.nextNode;
+    }
+    str += tempNode;
+    return str;
+  };
+
+  const insertAt = (val, index) => {
+    if (headNode === null) return null;
+
+    const valAtIndex = at(index);
+    const sizeLinkedList = size();
+    const sizeLinkedListMinusOne = sizeLinkedList - 1;
+    let cur = headNode;
+    let prev = null;
+
+    if (index < 0 || index > sizeLinkedListMinusOne) {
+      return console.log(
+        "Index start at 0, " +
+          "and the index must be within " +
+          "the bounds of the linked list size."
+      );
+    }
+
+    if (cur.value === valAtIndex) {
+      prepend(val);
+      return;
+    }
+
+    while (cur !== null && cur.value !== valAtIndex) {
+      prev = cur;
+      cur = cur.nextNode;
+    }
+
+    if (cur !== null) {
+      prev.nextNode = CreateNode(val, cur);
+    }
+  };
+
+  const removeAt = (index) => {
+    if (headNode === null) {
+      return console.log("Cannot delete, linked list is empty");
+    }
+
+    const valAtIndex = at(index);
+    const sizeLinkedList = size();
+    const sizeLinkedListMinusOne = sizeLinkedList - 1;
+    let cur = headNode;
+    let prev = null;
+
+    if (index < 0 || index > sizeLinkedListMinusOne) {
+      return console.log(
+        "Index start at 0, " +
+          "and the index must be within " +
+          "the bounds of the linked list size."
+      );
+    }
+
+    if (index === 0) {
+      headNode = headNode.nextNode;
+      return;
+    }
+
+    while (cur !== null && cur.LLvalue.key !== valAtIndex) {
+      prev = cur;
+      cur = cur.nextNode;
+    }
+
+    if (cur !== null) {
+      prev.nextNode = cur.nextNode;
+    }
+  };
+
+  const valueLinkedLists = () => headNode;
+
+  const removeLists = () => (headNode = null);
+
+  const logKeyLL = () => {
+    let tempNode = headNode;
+    let str = "";
+    while (tempNode !== null) {
+      str += `( ${tempNode.LLvalue.key} ) -> `;
+      tempNode = tempNode.nextNode;
+    }
+    str += tempNode;
+    return str;
+  };
+
+  const searchKey = (keyss) => {
+    let tempNode = headNode;
+    let str = "";
+
+    while (tempNode !== null) {
+      if (tempNode.LLvalue.key === keyss) {
+        return (str = tempNode.LLvalue.key);
+      }
+      str = tempNode.LLvalue.key;
+      tempNode = tempNode.nextNode;
+    }
+    str = tempNode;
+    return str;
+  };
+
+  return {
+    append,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+    valueLinkedLists,
+    removeLists,
+    logKeyLL,
+    searchKey,
+  };
+}
+
+function CreateNode(valueNode = null, valueNextNode = null) {
+  let LLvalue = valueNode;
+  let nextNode = valueNextNode;
+  return {
+    LLvalue,
+    nextNode,
+  };
+}
+
+export { LinkedList };
