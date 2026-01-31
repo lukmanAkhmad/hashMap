d3e8f8ef2c41b507cd55d079799ea46cb3def13d
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2026-01-31 15:33:25 +0700

    feat: implement HashMap

---
+++ main.js
@@ -0,0 +1,34 @@
+import { HashMap } from "./hashMap.js";
+
+const test = HashMap();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+console.log(test.length());
+test.set("moon", "silver");
+console.log(test.length());
+console.log(test.showBuckets());
+test.set("moon", "grey");
+console.log(test.length());
+console.log(test.showBuckets());
+console.log(test.get("carrot"));
+console.log(test.has("dog"));
+console.log(test.remove("moon"));
+console.log(test.length());
+console.log(test.showBuckets());
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+test.clear();
+console.log(test.length());
+console.log(test.showBuckets());
